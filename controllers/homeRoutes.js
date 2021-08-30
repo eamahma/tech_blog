0b549487d158a85541b972484aca5d804872a6cb
eamahma <eamahma@gmail.com>
2021-08-30 16:31:43 -0400

    set homepage handlebar to read from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,23 @@
 const router = require('express').Router();
+const apiRouter = require('./api');
 const { Note, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.use('/api', apiRouter);
+
 router.get('/', async (req, res) => {
-  try {
     // Get all notes and JOIN with user data
-    const noteData = await Note.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const noteData = await Note.findAll().catch((err) => {
+      res.json(err);
     });
 
     // Serialize data so the template can read it
     const notes = noteData.map((note) => note.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    // res.render('homepage', { 
-    //   projects, 
-    //   logged_in: req.session.logged_in 
-    // });
-    res.status(200).json(notes);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//    res.status(200).json(notes);
+
+//    Pass serialized data and session flag into template
+    res.render('homepage', { notes });
 });
 
 router.get('/note/:id', async (req, res) => {
@@ -40,12 +32,13 @@ router.get('/note/:id', async (req, res) => {
     });
 
     const note = noteData.get({ plain: true });
-
-    // res.render('note', {
-    //   ...project,
-    //   logged_in: req.session.logged_in
-    // });
     res.status(200).json(note);
+     res.render('note', {
+      ...project
+      // ...project,
+      // logged_in: req.session.logged_in
+    });
+
   } catch (err) {
     res.status(500).json(err);
   }
