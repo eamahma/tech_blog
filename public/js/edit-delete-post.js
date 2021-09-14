d8c8cf7cc647cad86a75d915d2f0dc8df720ddd2
eamahma <eamahma@gmail.com>
2021-09-13 21:02:16 -0400

    edit post fuction updated

--- public/js/edit-delete-post.js
+++ public/js/edit-delete-post.js
@@ -19,4 +19,40 @@ async function deletePostHandler() {
     }
 }
 
+async function editFormHandler(event) {
+    event.preventDefault();
+    console.log("edit button clicked");
+    // const title = document.querySelector('input[name="title"]').value.trim();
+    const title = document.getElementById('post-title').value.trim();
+    // const body = document.querySelector('input[name="body"]').textContent.trim();
+    const body = document.getElementById('post-body').value.trim();
+    console.log(title);
+    console.log(body);
+
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+      
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id,
+          title,
+          body
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+
+}
+
+document.querySelector('#edit-btn').addEventListener('click', editFormHandler);
+
 document.querySelector('#delete-btn').addEventListener('click', deletePostHandler);
